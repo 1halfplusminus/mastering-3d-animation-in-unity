a54324cc092b4be754a7c2f0a8b4a7661371a645
dartuchiwa <dartuchiwa@gmail.com>
2020-01-26 18:51:29 +1100

    Feat - Section 3

---
+++ Section3/Assets/CreatingWalkIdle/Y_Bot/Script/Drive.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum State
+{
+    Walking = 1,
+    Idle = 0,
+}
+public class Drive : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public float rotationSpeed = 100.0f;
+    public float jump = 1.0f;
+    int playerState;
+
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+        if (translation != 0)
+        {
+            animator.SetInteger("state", (int)State.Walking);
+            animator.SetFloat("speed", translation);
+        }
+        else
+        {
+            animator.SetInteger("state", (int)State.Idle);
+            animator.SetFloat("speed", translation);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jumping");
+        }
+    }
+}
