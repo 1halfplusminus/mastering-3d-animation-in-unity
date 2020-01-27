cdc8f8a0146e1064964dd931d10cc44afb66c61e
dartuchiwa <dartuchiwa@gmail.com>
2020-01-27 21:04:35 +1100

    Feat - Swimming Challenge

---
+++ Section4/Assets/SwimmingChallengeAnimation/Swim.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swim : MonoBehaviour
+{
+
+    float speed = 5.0f;
+    float rotationSpeed = 100.0f;
+
+    public float animationSpeed = 0.0f;
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        animator.SetFloat("animationSpeed", translation);
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+
+        if (translation != 0)
+        {
+            animator.SetBool("isSwimming", true);
+        }
+        else
+        {
+            animator.SetBool("isSwimming", false);
+        }
+    }
+}
---
+++ Section4/Assets/SwimmingChallengeAnimation/WaterPhys.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPhys : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("entered");
+        other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+        other.gameObject.GetComponent<Swim>().enabled = true;
+        other.gameObject.GetComponent<Drive>().enabled = false;
+        other.gameObject.GetComponent<Animator>().SetBool("isInWater", true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exit");
+        other.gameObject.GetComponent<Rigidbody>().useGravity = true;
+        other.gameObject.GetComponent<Swim>().enabled = false;
+        other.gameObject.GetComponent<Drive>().enabled = true;
+        other.gameObject.GetComponent<Animator>().SetBool("isInWater", false);
+        other.gameObject.GetComponent<Animator>().SetBool("isSwimming", false);
+        other.gameObject.transform.Translate(0, 0, 1);
+    }
+}
